f5ce610b07a9a5848e9ee9848a17e8bebc61eed0
David Cruz <ne0003021@enel.com>
2022-01-31 13:27:47 +0100

    various

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './css/index.css';
-import json from './marker_world.json';
-
+import json from './20220131_marker_world.json';
 let arrResults = [];
-const urlRemoteMap = 'https://mapa.shibacraft.net/tiles/_markers_/marker_world.json';
+
+// let json = {};
+// const urlRemoteMap = 'https://mapa.shibacraft.net/tiles/_markers_/marker_world.json';
 
 const parseMap = (jsonData) => {
   const areas = jsonData.sets['griefdefender.markerset'].areas;
@@ -31,7 +32,7 @@ const getRemoteMap = async (url) => {
   }
 };
 
-const mapPreview = (url) => `<div class="claim-map"><iframe src="${url}"></iframe></div>`;
+const mapPreview = (url) => `<iframe src="${url}"></iframe>`;
 
 const filterMap = ({ mapData, numDays = 5 }) => {
   const parser = new DOMParser();
@@ -52,8 +53,14 @@ const filterMap = ({ mapData, numDays = 5 }) => {
     const [w, m, d, h, t, y] = dateStr.split(' ');
     const fechaStr = `${d} ${m} ${y} ${h}`;
     const fecha = Date.parse(fechaStr);
-    const diffTime = calculateTime({ claimDate: fecha, interval: timeInterval });
-    const claimSize = calculateClaimSize({ x: e.x, z: e.z });
+    const diffTime = calculateTime({
+      claimDate: fecha,
+      interval: timeInterval,
+    });
+    const claimSize = calculateClaimSize({
+      x: e.x,
+      z: e.z,
+    });
     if (diffTime < 0) {
       const mapUrl = buildLink({
         x: e.x,
@@ -66,9 +73,9 @@ const filterMap = ({ mapData, numDays = 5 }) => {
         <li class="claim">
           <div class="claim-info">
             User: ${e.label}<br/>
-            Coords: <a href="${mapUrl}" target="_blank">${printCoords({ x: e.x, z: e.z })}</a><br/>
+            Coords: <a href="${mapUrl}" class="open_modal" target="_blank">${printCoords({ x: e.x, z: e.z })}</a><br/>
             Tamaño claim: ${claimSize}<br/>
-            Caducidad: ${calculateDeadlineDate(fecha).toDateString()}<br/>
+            Caducidad: ${calculateDeadlineDate(fecha).toString()}<br/>
             Info: ${e.desc}
           </div>
           <!-- ${mapPreview(mapUrl)} -->
@@ -77,12 +84,15 @@ const filterMap = ({ mapData, numDays = 5 }) => {
     }
   }
 
-  orderResults({ key: 'date', order: 'desc' });
+  orderResults({
+    key: 'date',
+    order: 'desc',
+  });
 };
 
 const orderResults = ({ key, order }) => {
   arrResults.sort((a, b) => {
-    if (order === 'desc') {
+    if (order === 'asc') {
       return a[key] - b[key];
     } else {
       return b[key] - a[key];
@@ -106,8 +116,6 @@ const calculateTime = ({ claimDate, interval }) => {
   const now = new Date().valueOf();
   const deadlineDate = now - 45 * 86400000;
   const limitDate = deadlineDate + interval;
-  // console.log({ now, interval, limitDate, claimDate });
-  // console.log(new Date(limitDate).toString());
   return claimDate - limitDate;
 };
 
@@ -124,38 +132,70 @@ const showResults = () => {
   document.getElementById('results').innerHTML = `<h3>${
     arrResults.length
   } resultados:</h3><ul>${printArrResults()}</ul>`;
+  bindMapLinks();
 };
 
 const cleanPreviousResults = () => {
   document.getElementById('results').innerHTML = '';
 };
 
+const bindMapLinks = () => {
+  const modalLinks = Array.from(document.getElementsByClassName('open_modal'));
+  modalLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      console.log(event.target);
+      const url = event.target.href;
+      openMap(url);
+    });
+  });
+};
+
 document.getElementById('search').addEventListener('click', () => {
   const numDays = document.getElementById('numDays').value;
+  // json = window.jsonImported;
   arrResults = [];
   cleanPreviousResults();
-  getRemoteMap(urlRemoteMap).then((json) => {
-    filterMap({ mapData: parseMap(json), numDays });
-    showResults();
+  // getRemoteMap(urlRemoteMap).then((json) => {
+  filterMap({
+    mapData: parseMap(json),
+    numDays,
   });
+  showResults();
+  // });
 });
 
 const buttons = Array.from(document.getElementsByClassName('order-button'));
 buttons.forEach((button) => {
   button.addEventListener('click', (evt) => {
-    let order = { key: '', order: '' };
+    let order = {
+      key: '',
+      order: '',
+    };
     switch (evt.target.id) {
       case 'order_date_asc':
-        order = { key: 'date', order: 'asc' };
+        order = {
+          key: 'date',
+          order: 'asc',
+        };
         break;
       case 'order_date_desc':
-        order = { key: 'date', order: 'desc' };
+        order = {
+          key: 'date',
+          order: 'desc',
+        };
         break;
       case 'order_size_asc':
-        order = { key: 'claimSize', order: 'asc' };
+        order = {
+          key: 'claimSize',
+          order: 'asc',
+        };
         break;
       case 'order_size_desc':
-        order = { key: 'claimSize', order: 'desc' };
+        order = {
+          key: 'claimSize',
+          order: 'desc',
+        };
         break;
     }
     cleanPreviousResults();
@@ -163,3 +203,27 @@ buttons.forEach((button) => {
     showResults();
   });
 });
+
+const openMap = (url) => {
+  const back = document.getElementById('modal_back');
+  const modal = document.getElementById('modal');
+  const content = document.getElementById('modal_content');
+
+  content.innerHTML = mapPreview(url);
+  back.style.display = 'block';
+  modal.style.display = 'flex';
+
+  modal.addEventListener('click', closeModal);
+  document.addEventListener('keydown', (evt) => {
+    const ESC = 27;
+    if (evt.keyCode === ESC) {
+      closeModal();
+    }
+  });
+};
+
+const closeModal = () => {
+  document.getElementById('modal_content').innerHTML = '';
+  document.getElementById('modal_back').style.display = 'none';
+  document.getElementById('modal').style.display = 'none';
+};
