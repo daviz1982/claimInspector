a5fa9a348870e60b7d24bd5eed4b3a4bbf1ae89f
David Cruz <daviz1982@gmail.com>
2023-01-08 15:58:18 +0100

    added users

--- src/users-shiba.js
+++ src/users-shiba.js
@@ -7,12 +7,28 @@ const getUserRange = username => {
 
   switch (username) {
     case 'JhonShepard':
+    case 'EllieDevil':
       return 1
     case 'jesusmpc':
+    case 'bru509':
+    case 'minecrafconedu':
       return 2
     case 'vladimirovich23':
+    case 'ZorrilloEstepa':
+    case 'Davido_SsS':
+    case 'Locurita':
+    case 'MrTroy77':
       return 3
     case 'JCC1995':
+    case '8Hiru8':
+    case 'oliver':
+    case 'Arparchancla':
+    case 'daviz667':
+    case 'G19TheGreat':
+    case 'Sagres_V':
+    case 'Celi511':
+    case 'JaviYoQueSe':
+    case 'GolfilloGTI':
       return 4
     default:
       return 0
