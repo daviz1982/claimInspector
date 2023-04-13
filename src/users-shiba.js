3cf79d5340dc6ea23dcda4c20dde33c766f8efd2
David Cruz <daviz1982@gmail.com>
2023-04-13 23:50:40 +0200

    updated usernames

--- src/users-shiba.js
+++ src/users-shiba.js
@@ -4,53 +4,53 @@ const getUserRange = username => {
   // const results = await collection.findOne({ username })
   // if (results) return results
   // return null
-  
-  /*
-  const daysToAbandon = {
-    0: 45, // Normal  - 200
-    1: 50, // Elite   - 400
-    2: 55, // Ultra   - 550
-    3: 60, // Mega    - 700
-    4: 65, // Supremo - 850
-  }
-  */
+
+  // 0: 45, // Normal  - 200
+  // 1: 50, // Elite   - 400
+  // 2: 55, // Ultra   - 550
+  // 3: 60, // Mega    - 700
+  // 4: 65, // Supremo - 850
 
   switch (username) {
-    case 'JhonShepard':
     case 'EllieDevil':
-    case 'LaChurryFlower':
+    case 'JhonShepard':
+    case 'Marietepete_07':
       return 1
-    case 'jesusmpc':
     case 'bru509':
+    case 'jesusmpc':
+    case 'LaChurryFlower':
     case 'minecrafconedu':
       return 2
+    case 'Davido_SsS':
+    case 'Frank1200tv':
+    case 'ladronderecursos':
+    case 'Locurita':
+    case 'LOLO23':
+    case 'Mario':
+    case 'MrTroy77':
+    case 'VBruno30':
     case 'vladimirovich23':
     case 'ZorrilloEstepa':
-    case 'Davido_SsS':
-    case 'Locurita':
-    case 'MrTroy77':
-    case 'vizucv':
-    case 'ladronderecursos':
-    case 'VBruno30':
       return 3
-    case 'JCC1995':
+    case '1_V3LF0R_1':
     case '8Hiru8':
-    case 'oliver':
     case 'Arparchancla':
-    case 'daviz667':
-    case 'G19TheGreat':
-    case 'Sagres_V':
     case 'Celi511':
-    case 'JaviYoQueSe':
+    case 'daviz667':
+    case 'deltafire777YT':
+    case 'G19TheGreat':
     case 'GolfilloGTI':
-    case 'Victxrius718':
+    case 'JaviYoQueSe':
+    case 'JCC1995':
+    case 'Lgm_16':
+    case 'oliver':
     case 'rociomlg96':
     case 'Unmalaje':
-    case 'Lgm_16':
+    case 'Sagres_V':
+    case 'Victxrius718':
+    case 'vizucv':
     case 'YTFocusMK4':
-    case 'deltafire777YT':
     case 'vayix':
-    case '1_V3LF0R_1':
       return 4
     default:
       return 0
