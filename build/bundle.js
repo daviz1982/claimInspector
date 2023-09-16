79da23a75a3cba5be7221ab0dbf88f5e0c8091a0
David Cruz <daviz1982@gmail.com>
2023-09-16 17:51:31 +0200

    add more changes for node 18

---
+++ build/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: error:0308010C:digital envelope routines::unsupported\n    at new Hash (node:internal/crypto/hash:69:19)\n    at Object.createHash (node:crypto:133:10)\n    at filename (D:\\Documentos\\code\\claimInspector\\node_modules\\babel-loader\\lib\\cache.js:94:23)\n    at D:\\Documentos\\code\\claimInspector\\node_modules\\babel-loader\\lib\\cache.js:120:39\n    at Generator.next (<anonymous>)\n    at asyncGeneratorStep (D:\\Documentos\\code\\claimInspector\\node_modules\\babel-loader\\lib\\cache.js:3:103)\n    at _next (D:\\Documentos\\code\\claimInspector\\node_modules\\babel-loader\\lib\\cache.js:5:194)\n    at D:\\Documentos\\code\\claimInspector\\node_modules\\babel-loader\\lib\\cache.js:5:364\n    at new Promise (<anonymous>)\n    at D:\\Documentos\\code\\claimInspector\\node_modules\\babel-loader\\lib\\cache.js:5:97");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
