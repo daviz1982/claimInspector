8e4311c729ab20af9efc035644e5dd9d9af2192c
David Cruz <daviz1982@gmail.com>
2023-10-15 02:10:14 +0200

    add users, lowercase users to avoid mess

--- src/users-shiba.js
+++ src/users-shiba.js
@@ -11,52 +11,62 @@ const getUserRange = username => {
   // 3: 60, // Mega    - 700
   // 4: 65, // Supremo - 850
 
-  switch (username) {
-    case 'EllieDevil':
-    case 'JhonShepard':
-    case 'Marietepete_07':
+  switch (username.toLowerCase()) {
+    case 'elliedevil':
+    case 'jhonshepard':
+    case 'marietepete_07':
       return 1
     case 'bru509':
     case 'jesusmpc':
-    case 'LaChurryFlower':
+    case 'lachurryflower':
     case 'minecrafconedu':
-    case 'Soldado_74':
+    case 'soldado_74':
       return 2
-    case 'Davido_SsS':
+    case 'davido_sss':
+    case 'eledechino':
     case 'ladronderecursos':
-    case 'Locurita':
-    case 'LOLO23':
-    case 'Mario':
-    case 'MrTroy77':
-    case 'Nievsassy':
-    case 'VBruno30':
+    case 'locurita':
+    case 'lolo23':
+    case 'mario':
+    case 'mrtroy77':
+    case 'nievsassy':
+    case 'vbruno30':
     case 'vladimirovich23':
-    case 'ZorrilloEstepa':
+    case 'zorrilloestepa':
       return 3
-    case '1_V3LF0R_1':
-    case '8Hiru8':
-    case 'Arparchancla':
-    case 'Celi511':
+    case '1_v3lf0r_1':
+    case '8hiru8':
+    case 'arparchancla':
+    case 'atkelo':
+    case 'brokenfj':
+    case 'bantic_4':
+    case 'celi511':
     case 'daviz667':
-    case 'deltafire777YT':
-    case 'Frank1200Tv':
-    case 'G19TheGreat':
-    case 'GolfilloGTI':
-    case 'Gorka35gg':
-    case 'JaviYoQueSe':
-    case 'JCC1995':
-    case 'Lgm_16':
+    case 'deltafire777yt':
+    case 'frank1200tv':
+    case 'g19thegreat':
+    case 'golfillogti':
+    case 'gorka35gg':
+    case 'javiyoquese':
+    case 'jcc1995':
+    case 'joseng':
+    case 'lgm_16':
+    case 'mimijaen':
     case 'oliver':
     case 'pinkroster68':
     case 'rociomlg96':
-    case 'Unmalaje':
-    case 'Sagres_V':
-    case 'SapSD':
+    case 'unmalaje':
+    case 'sagres_v':
+    case 'sapsd':
+    case 'sergiius':
+    case 'trripe1':
+    case 'turly04':
     case 'vayix':
-    case 'Victxrius718':
+    case 'victxrius718':
     case 'vizucv':
-    case 'Wito8':
-    case 'YTFocusMK4':
+    case 'wito8':
+    case 'youdontmeanmuch':
+    case 'ytfocusmk4':
       return 4
     default:
       return 0
